4c030252222fb5b6126d3e7b90db1ad16c8e928d
Simon Khawam <khawam.simon@me.com>
2023-11-12 21:12:55 +0200

    commit home-work-07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const listEl = document.querySelector(".gallery");
 
+galleryItems.forEach((item) => {
+    const listItemEl = document.createElement("li");
+    listItemEl.classList.add("gallery__item");
+    listItemEl.innerHTML = `<a class='gallery__link' href='${item.original}'>
+        <img class='gallery__image'
+        src='${item.preview}'
+        data-source='${item.original}'
+        alt='${item.description}'/>
+    </a>`;
+    listEl.append(listItemEl);
+});
+
+listEl.addEventListener("click", openImageInLightbox);
+
+function openImageInLightbox(event) {
+    const clickedOn = event.target;
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    event.preventDefault();
+
+    const lightbox = new SimpleLightbox(".gallery a", {});
+}
 console.log(galleryItems);
